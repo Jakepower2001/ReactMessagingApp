56604eb37fd2ebb35fce7fc47122d68d6cca2561
jake power <jakejosephpower@gmail.com>
2025-02-27 18:35:53 +0000

    Added a login page and set up the start of my toastify intergation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import Chat from "./components/chat/Chat"
-import List from "./components/list/List"
-import Detail from "./components/detail/Detail"
+import Chat from "./components/chat/Chat";
+import List from "./components/list/List";
+import Detail from "./components/detail/Detail";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+
+
 
 const App = () => {
+
+  const user = false;
+
   return (
     <div className='container'>
-    <List/>
-    <Chat/>
-    <Detail/>
-  </div>
-    
-  )
-}
+      {user ? (
+          <>
+            <List />
+            <Chat />
+            <Detail />
+          </>
+        ) : (
+        <Login/>
+      )}
+      <Notification/>
+    </div>
+      
+  );
+};
 
-export default App
+export default App;
