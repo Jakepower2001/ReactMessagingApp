bf4363308d14037669c7cd3f88d05c6ed0a69207
jake power <jakejosephpower@gmail.com>
2025-04-04 13:19:58 +0100

    started the login/signup functionality component, user can sign in and sign up with stored details, next commit will have the start of my state management implementation

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../../lib/firebase";
 import { doc, setDoc } from "firebase/firestore";
 const Login = () => {
@@ -10,6 +10,7 @@ const Login = () => {
         url:""
     })
 
+    const [loading,setLoading] = useState(false)
 
     const handleAvatar = e =>{
             if(e.target.files[0]){
@@ -22,6 +23,7 @@ const Login = () => {
 
     const handleRegister = async (e) =>{
         e.preventDefault()
+        setLoading(true)
         const formData = new FormData(e.target);
 
         const { username, email, password } = Object.fromEntries(formData)
@@ -29,10 +31,12 @@ const Login = () => {
     try{
 
         const res = await createUserWithEmailAndPassword(auth,email,password)
+        const imgUrl = await upload(avatar.file)
 
         await setDoc(doc(db, "Users", res.user.uid), {
             username,
             email,
+            avatar: imgUrl,
             id: res.user.uid,
             blocked:[],
         
@@ -45,13 +49,30 @@ const Login = () => {
     }catch(err){
         console.log(err)
         toast.error(err.message)
+    } finally{
+        setLoading(false)
     }
     };
     
 
 
-    const handleLogin = e =>{
+    const handleLogin = async (e) =>{
         e.preventDefault()
+        setLoading(true);
+        const formData = new FormData(e.target);
+
+        const {  email, password } = Object.fromEntries(formData)
+
+        try{
+            await signInWithEmailAndPassword(auth,email,password);
+        }
+        catch(err){
+            console.log(err)
+            toast.error(err.message)
+        }
+        finally{
+            setLoading(false)
+        }
     }
 
     return ( 
@@ -61,7 +82,7 @@ const Login = () => {
             <form onSubmit={handleLogin}>
                 <input type = "text" placeholder="Email" name="email"/>
                 <input type = "password" placeholder="Password" name="password"/>
-                <button>Sign In</button>
+                <button disabled={loading}>{loading ? "Loading" : "Sign In"}</button>
             </form>
         </div>
         <div className="separator"></div>
@@ -81,7 +102,7 @@ const Login = () => {
                 <input type = "text" placeholder="Username" name="username"/>
                 <input type = "text" placeholder="Email" name="email"/>
                 <input type = "password" placeholder="Password" name="password"/>
-                <button>Sign In</button>
+                <button disabled={loading}>{loading ? "Loading" : "Sign Up"}</button>
             </form>
             </div>
     </div>
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,20 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBRSj1_xAu-7dDU7flEN9qQ9l2s2d1mQ9w",
   authDomain: "test-dbf2d.firebaseapp.com",
