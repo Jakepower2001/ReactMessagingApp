0de97fcc734726f6a98aadab3bf067d48ee7caf3
jake power <jakejosephpower@gmail.com>
2025-04-08 20:41:00 +0100

    fixed issue with upload

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -1,31 +1,32 @@
+import { getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { storage } from "./firebase";
+
 const upload = async (file)=>{
 
-
-const storageRef = ref(storage, 'images/rivers.jpg');
+const date = new Date()
+const storageRef = ref(storage, `images/${date + file.name}`);
 
 const uploadTask = uploadBytesResumable(storageRef, file);
 
-uploadTask.on('state_changed', 
+return new Promise((resolve,reject)=>{
+
+uploadTask.on(
+  'state_changed', 
   (snapshot) => {
     const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
     console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-      case 'paused':
-        console.log('Upload is paused');
-        break;
-      case 'running':
-        console.log('Upload is running');
-        break;
-    }
+
   }, 
   (error) => {
+    reject("Something went wrong!" +error.code)
   }, 
   () => {
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log('File available at', downloadURL);
+    getDown(uploadTask.snapshot.ref).then((downloadURL) => {
+  resolve(downloadURL)
     });
   }
 );
-}
+});
+};
 
 export default upload;
