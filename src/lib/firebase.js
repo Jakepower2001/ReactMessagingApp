967f3dc9de515a89f6db798059c47583de9c1a69
jake power <jakejosephpower@gmail.com>
2025-04-26 23:55:44 +0100

    test commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, getDoc, doc } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 
@@ -15,8 +15,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth()
-export const db = getFirestore()
+export const db = getFirestore(app)
 export const storage = getStorage()
+
--- src/lib/upload.js
+++ src/lib/upload.js
@@ -13,7 +13,8 @@ return new Promise((resolve,reject)=>{
 uploadTask.on(
   'state_changed', 
   (snapshot) => {
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    const progress = 
+    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
     console.log('Upload is ' + progress + '% done');
 
   }, 
@@ -22,7 +23,7 @@ uploadTask.on(
   }, 
   () => {
     getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-  resolve(downloadURL)
+  resolve(downloadURL);
     });
   }
 );
